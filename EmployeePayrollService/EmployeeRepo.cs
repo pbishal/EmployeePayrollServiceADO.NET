ba99f8d9f6cbf7fd95959bf5b660027f93ba0490
pbishal <pbishal60@gmail.com>
2021-05-22 09:43:19 +0530

    Welcome to ADO.NET

---
+++ EmployeePayrollService/EmployeeRepo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Text;
+
+namespace EmployeePayrollService
+{
+    class EmployeeRepo
+    {
+        public static string conectionString = @"Data Source=BISHAL\SQLBISHAL;Initial Catalog=payroll_service;Persist Security Info=True;User ID=sa;Password=***********";
+        SqlConnection connection = new SqlConnection(conectionString);
+
+    }
+}
