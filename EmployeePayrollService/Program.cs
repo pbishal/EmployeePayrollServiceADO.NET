616025291e7fe728cb2249085a86749441f82931
pbishal <pbishal60@gmail.com>
2021-05-22 22:16:17 +0530

    Welcome to ADO.net

--- EmployeePayrollService/Program.cs
+++ EmployeePayrollService/Program.cs
@@ -10,6 +10,7 @@ namespace EmployeePayrollService
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Employee Payroll Service ADO.NET");
         }
     }
 }
